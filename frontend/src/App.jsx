5c70f1513f9c66350a76eda8da266d3657feddcd
Vincent <farhanalireza50@gmail.com>
2025-04-03 12:35:35 +0530

    added dynamic greetings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,28 +6,22 @@ import SpotifyConnect from "./Components/SpotifyConnect";
 import FirstTimeLogin from "./Components/FirstTimeLogin";
 import AppBar from "./Components/AppBar";
 
+function greetBasedOnTime() {
+    const now = new Date();
+    const hour = now.getHours();
+  
+    if (hour >= 5 && hour < 12) {
+      return ("Good Morning, ");
+    } else if (hour >= 12 && hour < 17) {
+      return ("Good Afternoon, ");
+    } else {
+      return ("Good Evening, ");
+    }
+  }
+  
+
 const SPOTIFY_TOKEN =
-  "BQDVOx4Mfc1Dz0kVat7p03OsWYWzeMeDhMu5XPHaeun99uW8TZ1Lh1_2IuEU09Nw-kJ6my5Oz3At6Vh349XdxvVK2xidyFWaQiXxmjzxfl47QbFO7FxzPrO-ukKwcHF9tdQrFmA8omo";
-// async function getUserPlaylists(accessToken) {
-//   const response = await fetch("https://api.spotify.com/v1/me/playlists", {
-//     method: "GET",
-//     headers: {
-//       Authorization: `Bearer ${accessToken}`,
-//       "Content-Type": "application/json",
-//     },
-//   });
-
-//   if (!response.ok) {
-//     throw new Error("Failed to fetch playlists");
-//   }
-
-//   const data = await response.json();
-//   console.log(data); 
-//   console.log(data.items);
-//   return data.items; 
-// }
-
-// getUserPlaylists(SPOTIFY_TOKEN);
+  "BQBCb-y5ekTWWeLyF05i0nrW_IN6oFm7xqu-uK8-LA7MfjNcn__LYLQqDYo3LSMiO8eXa6eAlpGapGOPMrBTt-3eKZgnDdwfQejtwfo4TIoU3sh0RHMIVvHwWoB2ycW_Jg76gGubG1s";
 
 async function fetchYouTubeData(title) {
   const response = await fetch(
@@ -178,7 +172,7 @@ function App() {
         <>
         <div className="flex flex-col justify-around md:flex-row">
           <h1 className="pt-5 pl-5 text-2xl dark:text-white m-1 mb-4 boldonse line-h line-clamp-3 md:text-4xl md:p-4" title={userName}>
-            Hello, {userName}
+            {greetBasedOnTime()} {userName}
           </h1>
           <h3 className="boldonse text-xs text-white m-3 flex justify-around items-center">
             Current Music Language: {musicLanguage}
