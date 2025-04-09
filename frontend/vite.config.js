cc929cb197c9245f4f845078c6e24d4749c1f2b5
Vincent <farhanalireza50@gmail.com>
2025-04-09 12:12:52 +0530

    updated app's manifest.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,43 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+    plugins: [
+        VitePWA({
+          registerType: 'autoUpdate',
+          includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+          manifest: {
+            name: 'My Awesome App',
+            short_name: 'AwesomeApp',
+            description: 'A dope app that plays music and changes lives',
+            theme_color: '#ffffff',
+            background_color: '#000000',
+            display: 'standalone',
+            start_url: '/',
+            icons: [
+              {
+                src: 'pwa-icon-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+              },
+              {
+                src: 'pwa-icon-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+              },
+            ],
+          },
+        }),
+        react(),
+        tailwindcss(),
+      ],
   server: {
     host: true, // Allows access from network devices
     // port: 5173, // (Optional) Change if needed
     // strictPort: true, // Ensures the port doesn’t change if 5173 is busy
   },
+  
 })
