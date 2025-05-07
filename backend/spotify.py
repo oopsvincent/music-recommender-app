1253e98445dc60b218a82fb275ce4e708fbbe171
Soumodip Mondal <164984170+soumodip-esc@users.noreply.github.com>
2025-05-07 20:03:19 +0530

    Update spotify.py

--- backend/spotify.py
+++ backend/spotify.py
@@ -13,7 +13,6 @@ spotify = Blueprint("spotify", __name__)
 CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
-# REDIRECT_URI = "http://127.0.0.1:5000/callback"
 
 access_token_cache = {"access_token": None, "expires_at": 0}
 
