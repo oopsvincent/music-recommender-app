0ea8d9d743e2144b1d57ca86619464e397c60eca
Soumodip Mondal <soumodipmondal40@gmail.com>
2025-08-16 10:07:08 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 CORS(
     app,
     origins=[
-        "https://music-recommender-app.vercel.app",
+        "https://grooveestrella.vercel.app/",
         "http://localhost:5173",
         "http://192.168.29.8:5173/",
     ],
